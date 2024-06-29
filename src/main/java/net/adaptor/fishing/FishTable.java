eb3d50eaa1aa880cd8139df312083396e9c50a2e
Yunitrish <yunthomas006@gmail.com>
2024-06-29 20:17:15 +0800

    namespace error fixed

--- src/main/java/net/adaptor/fishing/FishTable.java
+++ src/main/java/net/adaptor/fishing/FishTable.java
@@ -55,7 +55,7 @@ public class FishTable {
     }
 
     public FishTable addEntityAttribute(EntityType<?> entityType, RegistryEntry<EntityAttribute> attribute, double amount, EntityAttributeModifier.Operation operation) {
-        EntityAttributeModifier modifier = new EntityAttributeModifier(Main.id("fishing_"+attribute.getIdAsString()+"_bonus"),amount,operation);
+        EntityAttributeModifier modifier = new EntityAttributeModifier(Main.id("fishing.bonus."+attribute.getIdAsString().replaceAll("minecraft:","")),amount,operation);
         if (attributeModifiers.containsKey(entityType)) {
             attributeModifiers.get(entityType).put(attribute, modifier);
         }
@@ -66,7 +66,7 @@ public class FishTable {
     }
 
     public FishTable addEntityAttribute(RegistryEntry<EntityAttribute> attribute, double amount, EntityAttributeModifier.Operation operation) {
-        EntityAttributeModifier modifier = new EntityAttributeModifier(Main.id("fishing_"+attribute.getIdAsString()+"_bonus"),amount,operation);
+        EntityAttributeModifier modifier = new EntityAttributeModifier(Main.id("fishing.bonus."+attribute.getIdAsString().replaceAll("minecraft:","")),amount,operation);
         for (Map.Entry<EntityType<?>,Map<RegistryEntry<EntityAttribute>, EntityAttributeModifier>> set:attributeModifiers.entrySet()) {
             set.getValue().put(attribute,modifier);
         }
